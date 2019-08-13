8479b9d0c0e6709fb5d67b37e33d69bc74472df7
jpierre-louis2 <47150282+jpierre-louis2@users.noreply.github.com>
2019-08-13 13:13:37 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import './Vigenere.js'
 import { adfgvx } from './ADFGVX.js'
 import { adfgx } from './ADFGX.js'
+import { twosquare } from './TwoSquare.js'
+import { foursquare } from './FourSquare.js'
+import { playfair } from './Playfair.js'
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      value: '111'
+      value: ''
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -29,9 +32,9 @@ class App extends Component {
         <br/>Keyword: {GFG.ciKey("00000000")}
         <br/>Key: {GFG.ciKeyword(this.state.value)}
         {matrix(5)}*/}
-        Letter Found: {adfgx.search("FA")}
-        <br/>Search Entered: {adfgx.searchValue()}
-        <br/>Keyword: {adfgx.keyword()}
+        Letter Found: {}
+        <br/>Search Entered: {adfgvx.start("keyword","FA")}
+        <br/>Keyword: {}
         {/*<br/>Value Recorded: {this.state.value.split("").reverse().join("")}
         <br/>Value Desired   <input type="text" value={this.state.value}
         onChange={this.handleChange} />*/}
