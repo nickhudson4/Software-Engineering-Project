358c2fce28f702b47e0a2925d2e7646a852418f2
jpierre-louis2 <47150282+jpierre-louis2@users.noreply.github.com>
2019-09-26 13:13:01 -0400

    Add files via upload

--- main_app/src/Components/Body/InnerBody/CipherArea.js
+++ main_app/src/Components/Body/InnerBody/CipherArea.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import './InnerBody.css';
 import MonoAlphSub from './CipherArea/MonoAlphSub';
 import RailFence from './CipherArea/RailFence';
+import NewADFGX from './CipherArea/NewADFGX';
+import NewADFGVX from './CipherArea/NewADFGVX';
+import NewFourSquare from './CipherArea/NewFourSquare';
+import NewTwoSquare from './CipherArea/NewTwoSquare';
+import NewPlayfair from './CipherArea/NewPlayfair';
+import NewVigenere from './CipherArea/NewVigenere';
 /*
     Renders specific component based on cipher chosen
 */
@@ -27,12 +33,42 @@ const GetCipher = (props) => {
       <RailFence/>
     );
   }
+  else if (props.whichCipher === 'adfgx'){
+    return (
+      <NewADFGX/>
+    );
+  }
+  else if (props.whichCipher === 'adfgvx'){
+    return (
+      <NewADFGVX/>
+    );
+  }
+  else if (props.whichCipher === 'four'){
+    return (
+      <NewFourSquare/>
+    );
+  }
+  else if (props.whichCipher === 'two'){
+    return (
+      <NewTwoSquare/>
+    );
+  }
+  else if (props.whichCipher === 'play'){
+    return (
+      <NewPlayfair/>
+    );
+  }
+  else if (props.whichCipher === 'vig'){
+    return (
+      <NewVigenere/>
+    );
+  }
   else { //Error return empty div
     return (
       <div></div>
     );
   }
-  
+
 }
 
 export default CipherArea;
