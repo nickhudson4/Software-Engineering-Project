419547b84037eb9aff46fc02280de4d8df34d3bf
jpierre-louis2 <47150282+jpierre-louis2@users.noreply.github.com>
2019-09-26 13:08:01 -0400

    Add files via upload

---
+++ main_app/src/Components/Body/InnerBody/CipherArea/NewPlayfair.js
@@ -0,0 +1,137 @@
+import React, { Component } from 'react';
+import './CipherArea.css';
+import Table from './Table';
+import { playfair } from './Playfair.js';
+
+/*
+    Renders specific component based on cipher chosen.
+    Actual cipher backend logic will come from here
+
+
+    thisisatestinput encrypted with 3 rails:
+
+t      i     e       n
+  h  s  s  t   s   i   p   t  => tienhsstsiptiatu
+   i     a       t       u
+
+*/
+
+class Playfair extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            keyword: "playfair", //Defualt number of rails
+            inputText: 'none',
+            mode: 'none'
+        }
+    }
+
+
+
+    render() {
+
+        return (
+            <div id="playfair_div" className="specific_cipher_class">
+                <h1>Playfair</h1>
+                <div id="input_textArea_div" className="input_text_area">
+                    <textarea className="inputOutput_textArea" id="input_textArea" placeholder="Input" rows="25" cols="80"></textarea>
+                    {/* <br/> */}
+                    <div id="actions_div_id" className="actions_div">
+                        <button type="button" onClick={this.encryptOnClick}>Encrypt</button>
+                        <button type="button" onClick={this.decryptOnClick}>Decrypt</button>
+
+                        <form className="textBox_form">
+                            Keyword: <input id="rails_input" className="input_textBox" type="text" min="2" step="1" name="num_rails" value={this.state.keyword} onChange={evt => this.updateKeyword(evt)}></input>
+                        </form>
+                    </div>
+
+
+                </div>
+                <LoadMode mode = {this.state.mode} inputKeyword = {this.state.keyword} inputText = {this.state.inputText}/>
+            </div>
+        );
+    }
+
+  encryptOnClick = () => {
+    var text = document.getElementById("input_textArea").value;  //Inputed text
+    this.setState({
+      inputText: text,
+      mode: 'encrypt'
+    })
+  }
+  decryptOnClick = () => {
+    var text = document.getElementById("input_textArea").value;  //Inputed text
+    this.setState({
+      inputText: text,
+      mode: 'decrypt'
+    })
+  }
+
+    updateKeyword(evt) { //Reload when number of rails is changed
+        this.setState({
+          keyword: evt.target.value
+        });
+    }
+}
+
+export default Playfair;
+
+
+const Encrypt = (props) => {
+ playfair.start()
+ var temp = playfair.encrypt(props.inputText)
+ return (
+     <OutputTextarea message={temp} />
+ );
+}
+
+
+
+const Decrypt = (props) => {
+  playfair.start()
+  var temp = playfair.decrypt(props.inputText)
+    return (
+        <OutputTextarea message={temp} />
+    );
+}
+
+
+
+var copyToClipboard = (e) => {
+    var textAreaToCopy = document.getElementById('output_textArea');
+    textAreaToCopy.select();
+    document.execCommand('copy');
+    e.target.focus();
+};
+
+const LoadMode = (props) => {
+    var mode = props.mode;
+    var inputKeyword = props.inputKeyword;
+    var inputText = props.inputText;
+
+    if (mode === 'encrypt'){
+        return (
+            <Encrypt inputKeyword = {inputKeyword} inputText = {inputText}/>
+        );
+    }
+    else if (mode === 'decrypt'){
+        return (
+            <Decrypt inputKeyword = {inputKeyword} inputText = {inputText}/>
+        );
+    }
+    else {
+        return (
+            <div></div>
+        );
+    }
+}
+
+/* Renders the text area with encrypted/decrypted string  */
+const OutputTextarea = (props) => {
+    return (
+        <div id="output_textArea_div" className="output_text_area">
+            <textarea className="inputOutput_textArea" id="output_textArea" value={props.message} placeholder="Output" rows="25" cols="80" onChange={() => {}}></textarea>
+            <button onClick={copyToClipboard}>Copy <br/> Output</button>
+        </div>
+    );
+}
---
+++ main_app/src/Components/Body/InnerBody/CipherArea/NewVigenere.js
@@ -0,0 +1,133 @@
+import React, { Component } from 'react';
+import './CipherArea.css';
+import Table from './Table';
+import { vigenere} from './Vigenere.js';
+
+/*
+    Renders specific component based on cipher chosen.
+    Actual cipher backend logic will come from here
+
+
+    thisisatestinput encrypted with 3 rails:
+
+t      i     e       n
+  h  s  s  t   s   i   p   t  => tienhsstsiptiatu
+   i     a       t       u
+
+*/
+
+class Vigenere extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            keyword: "vigenere", //Defualt number of rails
+            inputText: 'none',
+            mode: 'none'
+        }
+    }
+
+
+
+    render() {
+
+        return (
+            <div id="vigenere_div" className="specific_cipher_class">
+                <h1>Vigenere</h1>
+                <div id="input_textArea_div" className="input_text_area">
+                    <textarea className="inputOutput_textArea" id="input_textArea" placeholder="Input" rows="25" cols="80"></textarea>
+                    {/* <br/> */}
+                    <div id="actions_div_id" className="actions_div">
+                        <button type="button" onClick={this.encryptOnClick}>Encrypt</button>
+                        <button type="button" onClick={this.decryptOnClick}>Decrypt</button>
+
+                        <form className="textBox_form">
+                            Keyword: <input id="rails_input" className="input_textBox" type="text" min="2" step="1" name="num_rails" value={this.state.keyword} onChange={evt => this.updateKeyword(evt)}></input>
+                        </form>
+                    </div>
+
+
+                </div>
+                <LoadMode mode = {this.state.mode} inputKeyword = {this.state.keyword} inputText = {this.state.inputText}/>
+            </div>
+        );
+    }
+
+  encryptOnClick = () => {
+    var text = document.getElementById("input_textArea").value;  //Inputed text
+    this.setState({
+      inputText: text,
+      mode: 'encrypt'
+    })
+  }
+  decryptOnClick = () => {
+    var text = document.getElementById("input_textArea").value;  //Inputed text
+    this.setState({
+      inputText: text,
+      mode: 'decrypt'
+    })
+  }
+
+    updateKeyword(evt) { //Reload when number of rails is changed
+        this.setState({
+          keyword: evt.target.value
+        });
+    }
+}
+
+export default Vigenere;
+
+
+const Encrypt = (props) => {
+ vigenere.start()
+ var temp = vigenere.encrypt(props.inputText)
+ return (
+     <OutputTextarea message={temp} />
+ );
+}
+
+
+
+const Decrypt = (props) => {
+  vigenere.start()
+}
+
+
+
+var copyToClipboard = (e) => {
+    var textAreaToCopy = document.getElementById('output_textArea');
+    textAreaToCopy.select();
+    document.execCommand('copy');
+    e.target.focus();
+};
+
+const LoadMode = (props) => {
+    var mode = props.mode;
+    var inputKeyword = props.inputKeyword;
+    var inputText = props.inputText;
+
+    if (mode === 'encrypt'){
+        return (
+            <Encrypt inputKeyword = {inputKeyword} inputText = {inputText}/>
+        );
+    }
+    else if (mode === 'decrypt'){
+        return (
+            <Decrypt inputKeyword = {inputKeyword} inputText = {inputText}/>
+        );
+    }
+    else {
+        return (
+            <div></div>
+        );
+    }
+}
+
+/* Renders the text area with encrypted/decrypted string  */
+const OutputTextarea = (props) => {
+    return (
+        <div id="output_textArea_div" className="output_text_area">
+            <textarea className="inputOutput_textArea" id="output_textArea" value={props.message} placeholder="Output" rows="25" cols="80" onChange={() => {}}></textarea>
+            <button onClick={copyToClipboard}>Copy <br/> Output</button>
+        </div>
+    );
+}
